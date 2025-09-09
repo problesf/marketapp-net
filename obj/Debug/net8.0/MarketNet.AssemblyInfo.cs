cc8c3b97de827dd126bb6fa3b35a0743205e1c75
pablo.robles <pablo.robles@xeridia.com>
2025-09-09 16:44:01 +0200

    Se completa la futura estructura del proyecto

--- obj/Debug/net8.0/MarketNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/MarketNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b01717bc301038c6715a0a5e32c9c9abba811992")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
